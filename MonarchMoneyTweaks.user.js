297484116684c9d20e6c80823f8524d6293f93f2
Robert P <163090599+RobertParesi@users.noreply.github.com>
2024-09-20 18:37:27 -0700

    Update MonarchMoneyTweaks.user.js

--- MonarchMoneyTweaks.user.js
+++ MonarchMoneyTweaks.user.js
@@ -1,19 +1,24 @@
 // ==UserScript==
 // @name         Monarch Money Tweaks
 // @namespace    http://tampermonkey.net/
-// @version      1.10
+// @version      1.11
 // @description  Monarch Tweaks
-// @author       Robert
+// @author       Robert P
 // @match        https://app.monarchmoney.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=monarchmoney.com
 // @grant        GM_addStyle
+// @grant        GM_info
 // ==/UserScript==
 
-const MM_Version = '1.10';
+const css_currency = 'USD';
+const graphql = 'https://api.monarchmoney.com/graphql';
+const pending = 2;
+
 let r_Init = false;
 let r_TipsActive = 0;
 let r_ToasterActive = 0;
 let r_DatePickerActive = false;
+let r_PlanGridActive = false;
 let r_Filter = 0;
 let r_FilterD = false;
 let r_spawn = 0;
@@ -21,12 +26,12 @@ let r_eventListener = false;
 let r_oo = null;
 let r_oo2 = null;
 let r_ooPO = null;
-const css_currency = 'USD';
+let r_ooPl = null;
+let r_ooPl2 = null;
+let r_PlanYear = '';
 let accountGroups = [];
-const graphql = 'https://api.monarchmoney.com/graphql';
-
+let SaveLocationHRefName = "";
 let SaveLocationPathName = "";
-
 
 function MM_Init() {
 
@@ -36,6 +41,9 @@ function MM_Init() {
         a1 = 'background-color: #ffffff; color: rgb(8, 40, 100);';
         a2 = 'background-color: #eaf6fd; color: rgb(61, 146, 222);';
     }
+
+    GM_addStyle('.MTPlanHeader {font-weight: 900; align-content: inherit; padding: 0px 0px 15px; }');
+    GM_addStyle('.MTPlanDetail {color: rgb(72, 157, 140);font-size: 16px; font-weight: 500;}');
 
     GM_addStyle('.MTlink {background-color: transparent; color: rgb(50, 170, 240); font-weight: 500; font-size: 14px; cursor: pointer; border-radius: 4px;  border-style: none; padding: 15px 1px 1px 16px; display:inline-block;}');
     GM_addStyle('.MTlink2 {background-color: transparent; font-size: 14px; font-weight: 500; padding: 0px 0px 0px 16px;}');
@@ -87,20 +95,42 @@ function MM_SetupCallbacks() {
 
     let useTarget=document.querySelector('body');
     if(useTarget != null) {
-        const observer2 = new MutationObserver(MM_SplitCallback);
-        const config = { attributes: false, childList: true, subtree: false };
+        const observer2 = new MutationObserver(MM_BodyCallback);
+        const config = { attributes: true, childList: true, subtree: true, };
         observer2.observe(useTarget, config);
         r_oo2 = useTarget;
     }
 }
+
+const MM_BodyCallback = (mutationList, observer2) => {
+
+    // recalculate Budget
+    if(r_PlanGridActive == pending) {
+        if (r_PlanYear != '') {
+            if(mutationList.length < 49) {
+                r_PlanGridActive = true;
+            }
+        }
+    }
+
+    // Split 50/50
+    if(r_oo2) {
+        let text = r_oo2.lastChild.innerText;
+        if(text) {
+            if(text.startsWith('Split Transaction')) {
+                MM_SplitTransaction()
+            }
+        }
+    }
+}
+
 // ===[ Reports Menu ] ===
 function MenuReports(OnFocus) {
 
-    if (SaveLocationPathName.substring(0,9) == '/reports/') {
+    if (SaveLocationPathName.startsWith('/reports/')) {
         if(OnFocus == false) {
             let pn = window.location.pathname;
-            let pi = pn.search('/reports/');
-            if(pi == -1) {
+            if(pn.startsWith('/reports/') == false) {
                 // leaving all reports
                 r_oo = null;
             };
@@ -162,8 +192,7 @@ function MenuFilter() {
         let ca = document.cookie.split(';');
         for(let i = 0; i < ca.length; i++) {
             let c = ca[i].trim();
-            let cm = c.substring(0,4);
-            if(cm == 'MTF_') {
+            if(c.startsWith('MTF_')) {
                 let ds = c.split('={');
                 rnames.push(ds[0]);
             }
@@ -459,17 +488,6 @@ function removeAllEncompass(InValue,InStart,InEnd) {
 }
 
 // ===[ Splits ] ======================================
-
-const MM_SplitCallback = (mutationList, observer2) => {
-
-    let text = r_oo2.lastChild.innerText;
-    if(text) {
-        if(text.substring(0,17) == 'Split Transaction') {
-            MM_SplitTransaction()
-        }
-    }
-}
-
 function MM_SplitTransaction() {
 
     let li = document.querySelector('[class*="TransactionSplitOriginalTransactionContainer__Amount"]')
@@ -507,7 +525,7 @@ function MM_SplitTransaction() {
 
 function MenuTransactions(OnFocus) {
 
-    if (SaveLocationPathName.substring(0,13) == '/transactions') {
+    if (SaveLocationPathName.startsWith('/transactions')) {
         if(OnFocus == false) {
             r_oo = null;
         }
@@ -519,27 +537,28 @@ function MenuTransactions(OnFocus) {
 
 function MenuDisplay(OnFocus) {
 
-    if (SaveLocationPathName.substring(0,17) == '/settings/display') {
+    if (SaveLocationPathName.startsWith('/settings/display')) {
         if(OnFocus == false) {
             // reload defaults
             r_Init = false;
         }
         if(OnFocus == true) {
-            MenuDisplay_Input('Monarch Money Tweaks - Version ' + MM_Version,'','header');
-            MenuDisplay_Input('Lowest Calendar Year','MT_LowCalendarYear','number');
-            MenuDisplay_Input('Hide Budget','MT_Budget','checkbox');
-            MenuDisplay_Input('Hide Recurring','MT_Recurring','checkbox');
-            MenuDisplay_Input('Hide Goals','MT_Goals','checkbox');
-            MenuDisplay_Input('Hide Investments','MT_Investments','checkbox');
-            MenuDisplay_Input('Hide Advice','MT_Advice','checkbox');
-            MenuDisplay_Input('Hide Monarch Ads','MT_Ads','checkbox');
-            MenuDisplay_Input('Hide Report Tooltip Difference','MT_HideTipDiff','checkbox');
-            MenuDisplay_Input('Hide Create Rule Popup','MT_HideToaster','checkbox');
-            MenuDisplay_Input('Calendar "Last year" and "Last 12 months" include full month','MT_CalendarEOM','checkbox');
-            MenuDisplay_Input('Transactions panel has smaller compressed grid (Requires refresh page)','MT_CompressedTx','checkbox');
-            MenuDisplay_Input('Hide Accounts Net Worth Graph Panel','MT_HideAccountsGraph','checkbox');
-            MenuDisplay_Input('Show Pending Transactions in Red','MT_PendingIsRed','checkbox');
-            MenuDisplay_Input('Add Drill-Down functionality to Reports Income/Spending','MT_ReportsDrilldown','checkbox');
+            MenuDisplay_Input(GM_info.script.name + ' - ' + GM_info.script.version,'','header');
+            MenuDisplay_Input('Lowest Calendar year','MT_LowCalendarYear','number');
+            MenuDisplay_Input('Menu - Hide Budget','MT_Budget','checkbox');
+            MenuDisplay_Input('Menu - Hide Recurring','MT_Recurring','checkbox');
+            MenuDisplay_Input('Menu - Hide Goals','MT_Goals','checkbox');
+            MenuDisplay_Input('Menu - Hide Investments','MT_Investments','checkbox');
+            MenuDisplay_Input('Menu - Hide Advice','MT_Advice','checkbox');
+            MenuDisplay_Input('Menu - Hide Monarch Ads','MT_Ads','checkbox');
+            MenuDisplay_Input('General - Calendar "Last year" and "Last 12 months" include full month','MT_CalendarEOM','checkbox');
+            MenuDisplay_Input('Accounts - Hide Accounts Net Worth Graph panel','MT_HideAccountsGraph','checkbox');
+            MenuDisplay_Input('Transactions - Transactions panel has smaller compressed grid (Requires refresh page)','MT_CompressedTx','checkbox');
+            MenuDisplay_Input('Transactions - Show Pending Transactions in red (Allow Pending Edits must be disabled)','MT_PendingIsRed','checkbox');
+            MenuDisplay_Input('Transactions - Hide Create Rule pop-up','MT_HideToaster','checkbox');
+            MenuDisplay_Input('Reports - Add Drill-Down functionality to Reports Income/Spending','MT_ReportsDrilldown','checkbox');
+            MenuDisplay_Input('Reports - Hide Chart Tooltip difference amount','MT_HideTipDiff','checkbox');
+            MenuDisplay_Input('Budget - Add YTD Total & Projected to Forecast Monthly & Export','MT_PlanYTD','checkbox');
         }
     }
 }
@@ -602,11 +621,285 @@ function MenuDisplay_Input(inValue,inCookie,inType) {
         }
     }
 }
+// ===== [ Plan & Budget ] ============================
+function MenuPlan(OnFocus) {
+
+    if (SaveLocationPathName.startsWith('/plan')) {
+        if(getCookie("MT_PlanYTD") == 1) {
+            if(OnFocus == false) {
+                r_PlanGridActive = false;
+                if(r_ooPl) {
+                    r_ooPl.remove();
+                    r_ooPl = null;
+                }
+                if(r_ooPl2) {
+                    r_ooPl2.remove();
+                    r_ooPl2 = null;
+                }
+            }
+            if(OnFocus == true) {
+                r_PlanGridActive = true;
+            }
+        }
+    }
+}
+
+function MenuPlanUpdate() {
+
+    // Getting Setting button of Plan container
+    let hed = document.querySelector('[class*="PlanHeaderControls__SettingsButton"]');
+    if(!hed) {
+        r_PlanYear = '';
+        r_PlanGridActive = pending;
+        return;
+    }
+
+    // Create buttons if needed
+    if(r_ooPl == null) {
+        let hed2 = document.querySelector('[class*="PlanHeaderControls__Root"]');
+        if(hed2) {
+
+            r_ooPl = document.createElement('button');
+            r_ooPl.className = "MT_PlanRecalc " + hed.className;
+            r_ooPl.textContent = 'Recalculate';
+            r_ooPl.style = 'display: ' + getDisplay(1)
+            hed2.prepend(r_ooPl);
+            r_ooPl.addEventListener('click', () => {
+                r_PlanGridActive = true;
+            });
+
+            let divI = document.createElement('div');
+            divI.setAttribute('class','PlanHeaderControls__Divider cwyGMK');
+            hed2.prepend(divI);
+
+            r_ooPl2 = document.createElement('button');
+            r_ooPl2.className = "MT_PlanExport " + hed.className;
+            r_ooPl2.textContent = 'Export';
+            r_ooPl2.style = 'display: ' + getDisplay(1)
+            hed2.prepend(r_ooPl2);
+            r_ooPl2.addEventListener('click', () => {
+                MenuPlanExport();
+            });
+        }
+    }
+
+    // turn off buttons (assume off)
+    if(r_ooPl) {r_ooPl.style = 'display: ' + getDisplay(1)};
+    if(r_ooPl2) {r_ooPl2.style = 'display: ' + getDisplay(1)};
+
+    // remove all tweaked columns
+    const headers = document.querySelectorAll('.MTPlanHeader');
+    const details = document.querySelectorAll('.MTPlanDetail');
+    headers.forEach(header => header.remove());
+    details.forEach(detail => detail.remove());
+
+    // check if forecasting year
+    hed = document.querySelector('[class*="PlanHeader__Container"]');
+    if(!hed) {
+        r_PlanYear = '';
+        r_PlanGridActive = pending;
+        return;
+    }
+
+    // get current page year
+    r_PlanYear = hed.innerText.substring(0,4);
+    if(hed.innerText[5] == '-') {
+        r_PlanGridActive = pending;
+        r_PlanYear = '';
+        return;
+    }
+
+    // wait till a full page load
+    let elements = document.querySelectorAll('[class*="PlanSectionFooter__Title"]');
+    if(elements == null || elements.length < 4) {
+        r_PlanYear = '';
+        r_PlanGridActive = true;
+        return;
+    }
+
+    if(r_ooPl2) {r_ooPl2.style = 'display: ' + getDisplay(0)};
+
+    // only tweak this year's forecast
+    const d = new Date();
+    let year = d.getFullYear();
+    let month = d.getMonth();
+    if(year.toString() != r_PlanYear) {
+        r_PlanGridActive = pending;
+        r_PlanYear = '';
+        return;
+    }
+
+    if(month > 0) {
+        // insert header
+        month-=1;
+        let currentPeriod = getMonthName(month,true) + ' ' + year;
+        let elements = document.querySelectorAll('[class*="PlanGrid__PlanGridColumn"]');
+
+        for (let el of elements) {
+            if(el.innerText == currentPeriod) {
+                let clonedNode = el.cloneNode(true);
+                clonedNode.innerText = 'Total YTD';
+                clonedNode.className = clonedNode.className + ' MTPlanHeader';
+                el.insertAdjacentElement('afterend', clonedNode);
+            }
+            if(el.innerText == 'Dec ' + year) {
+                let clonedNode = el.cloneNode(true);
+                clonedNode.innerText = 'Projected';
+                clonedNode.className = clonedNode.className + ' MTPlanHeader';
+                el.insertAdjacentElement('afterend', clonedNode);
+            }
+        }
+
+        // add data
+        let pivotCount = 0;
+
+        var cellValue = 0;
+        var rowValue = 0;
+        let useValue = '';
+        let clonedNodeYTD = null;
+        let clonedNodeProj = null;
+
+        month+=1;
+
+        elements = document.querySelectorAll('[class*="PlanGrid__PlanGridColumn"]');
+        if(elements) {
+        }
+
+        for (let el of elements) {
+            useValue = el.innerText;
+            if(!useValue) {
+                let x = el.lastChild.childNodes[0];
+                if(x) {
+                    useValue = x.defaultValue;
+                }
+            }
+            // check half page load
+            if(useValue == null || useValue == '-') {
+                r_PlanGridActive = pending;
+                return;
+            }
+            if(useValue.startsWith('$') || useValue.startsWith('-')) {
+                cellValue = getCleanValue(useValue);
+                pivotCount+=1;
+                rowValue += cellValue;
+
+                // clone first column
+                if(pivotCount == 1) {
+                    clonedNodeYTD = el.cloneNode(true);
+                    clonedNodeYTD.className = clonedNodeYTD.className + ' MTPlanDetail';
+                    clonedNodeProj = el.cloneNode(true);
+                    clonedNodeProj.className = clonedNodeProj.className + ' MTPlanDetail';
+                };
+
+                if(pivotCount == month) {
+                    if(isNaN(rowValue)) {
+                        clonedNodeYTD.innerText = '-';
+                    } else {
+                        clonedNodeYTD.innerText = rowValue.toLocaleString("en-US", {style:"currency", currency:css_currency});
+                    };
+                    el.insertAdjacentElement('afterend', clonedNodeYTD);
+                };
+
+                if(pivotCount == 12) {
+                    if(isNaN(rowValue)) {
+                        clonedNodeProj.innerText = '-';
+                    } else {
+                        clonedNodeProj.innerText = rowValue.toLocaleString("en-US", {style:"currency", currency:css_currency});
+                    };
+                    el.insertAdjacentElement('afterend', clonedNodeProj);
+                    pivotCount = 0;
+                    rowValue = 0;
+                };
+            }
+        }
+    }
+
+    if(r_ooPl) {r_ooPl.style = 'display: ' + getDisplay(0)};
+    if(r_ooPl2) {r_ooPl2.style = 'display: ' + getDisplay(0)};
+
+    r_PlanGridActive = pending;
+ }
+
+function MenuPlanExport(Year) {
+
+    const CRLF = String.fromCharCode(13,10);
+
+    const spans = document.querySelectorAll('[class*="PlanRowTitle__Title"]');
+    const data = [];
+    const spans2 = document.querySelectorAll('[class*="PlanCellAmount"], [class*="PlanCell__Amount"]');
+    const data2 = [];
+
+    let useValue = '';
+
+    // Headers
+    spans.forEach(span => {
+        if(span.firstChild.title || span.innerText.startsWith('Show ') || span.innerText.startsWith('Collapse ')) {
+           // hide data
+        } else
+        {
+            data.push(span.innerText);
+        }
+    });
+    // Cell Amounts
+    const storedStyle = spans2[0].className;
+    spans2.forEach(span => {
+        if(span.className == storedStyle) {
+            useValue = span.innerText;
+            useValue = getCleanValue(useValue);
+            data2.push(useValue);
+        }
+    });
+
+    var i = 0;
+    var ii = 0;
+    var iir = 0;
+    let FullRow = r_PlanYear;
+
+    for (i = 0; i < 12; i++) {
+        FullRow = FullRow + ',' + getMonthName(i,true);
+    }
+
+    let csvContent = FullRow + CRLF;
+
+    for (i = 0; i < data2.length; i++) {
+        if(iir == 0) {
+            if(data[ii] == 'Goals') {
+                break;
+            };
+            FullRow = '"' + data[ii] + '"' ;
+            ii+=1;
+        }
+
+        iir+=1;
+        FullRow = FullRow + ',' + data2[i];
+
+        if(iir == 12) {
+            iir = 0;
+           csvContent = csvContent + FullRow + CRLF;
+        }
+    };
+
+    downloadFile('Forecast Plan',csvContent);
+
+}
 
 function MenuCheckSpawnProcess() {
 
     if(r_DatePickerActive == true) {
         MM_FixCalendarYears();
+    }
+
+    switch (r_PlanGridActive) {
+        case true:
+            MenuPlanUpdate();
+            break;
+        case pending:
+            if(SaveLocationHRefName != window.location.href) {
+                // reload plan
+                SaveLocationHRefName = window.location.href;
+                r_PlanGridActive = true;
+            }
+            break;
     }
 
     if(r_spawn > 0) {
@@ -669,6 +962,28 @@ function inputTwoFields(InSelector,InValue1,InValue2) {
             x[1].value = '';
             document.execCommand('insertText', false, InValue2);
         }
+    }
+}
+
+function getCleanValue(inValue) {
+
+    if(inValue) {
+
+        let AmtStr = inValue.replace(/[$,]+/g,"");;
+        let Amt = parseInt(AmtStr);
+        return Amt;
+
+    } else {return null};
+}
+
+function getMonthName(inValue,inShort) {
+
+    const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+    if(inShort == true) {
+        return months[inValue].substring(0,3);
+    } else
+    {
+        return months[inValue];
     }
 }
 
@@ -785,6 +1100,19 @@ function getDisplay(InA) {
     if(InA == 1) {return 'none;'} else {return 'block;'};
 }
 
+function downloadFile(inTitle,inData) {
+
+    const encodedUri = encodeURI('data:text/csv;charset=utf-8,' + inData);
+    const link = document.createElement('a');
+    link.setAttribute('href', encodedUri);
+    link.setAttribute('download', inTitle + '.csv');
+    document.body.appendChild(link);
+
+    link.click();
+
+    document.body.removeChild(link);
+}
+
 (function() {
 
     setInterval(() => {
@@ -802,14 +1130,17 @@ function getDisplay(InA) {
                     MenuReports(false);
                     MenuDisplay(false);
                     MenuTransactions(false);
+                    MenuPlan(false)
                 };
 
                 SaveLocationPathName = window.location.pathname;
+                SaveLocationHRefName = window.location.href;
 
                 // Gain Focus on a Page
                 MenuReports(true);
                 MenuDisplay(true);
                 MenuTransactions(true);
+                MenuPlan(true);
             };
 
             MenuCheckSpawnProcess();
