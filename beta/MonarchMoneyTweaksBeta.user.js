ae304691267a9a87e768034f4e617e02f0b80e4b
Robert P <163090599+RobertParesi@users.noreply.github.com>
2024-11-18 08:55:48 +1100

    Update MonarchMoneyTweaksBeta.user.js
    Signed-off-by: Robert P <163090599+RobertParesi@users.noreply.github.com>

--- beta/MonarchMoneyTweaksBeta.user.js
+++ beta/MonarchMoneyTweaksBeta.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         Monarch Money Tweaks
 // @namespace    http://tampermonkey.net/
-// @version      2.03.03
+// @version      2.03.04
 // @description  Monarch Tweaks
 // @author       Robert P
 // @match        https://app.monarchmoney.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=monarchmoney.com
 // ==/UserScript==
 
-const version = '2.03.03';
+const version = '2.03.04';
 const css_currency = 'USD';
 const css_green = 'color: #489d8c;';
 const css_red = 'color: #ed5987;';
@@ -659,7 +659,7 @@ async function MenuReportsAccountsGo() {
     MTFlex.TriggerEvent = true;
     MTFlex.TriggerEvents = false;
     MTFlex.Button1Options = ['Hide subtotals','Show subtotals'];
-    MTFlex.Button2Options = ['This week','This month','3 months', '6 months', 'This year', '1 year', '2 years', '3 years'];
+    MTFlex.Button2Options = ['Yesterday','This week','This month','3 months', '6 months', 'This year', '1 year', '2 years', '3 years'];
     MTFlex.Subtotals = MTFlex.Button1;
 
     let isToday = getDates('isToday',AccountsTodayIs);
@@ -668,7 +668,7 @@ async function MenuReportsAccountsGo() {
     let snapshotData = null;
     let snapshotData2 = null;
     let snapshotData3 = null;
-    let useDateRange = ['d_MinusWeek','d_StartofMonth','d_Minus3Months','d_Minus6Months','d_StartOfYear','d_Minus1Year','d_Minus2Years','d_Minus3Years'][MTFlex.Button2];
+    let useDateRange = ['d_Yesterday','d_MinusWeek','d_StartofMonth','d_Minus3Months','d_Minus6Months','d_StartOfYear','d_Minus1Year','d_Minus2Years','d_Minus3Years'][MTFlex.Button2];
     let useDate = getDates(useDateRange,AccountsTodayIs);
     let useDate2 = AccountsTodayIs;
     let cards = 0;
@@ -680,7 +680,7 @@ async function MenuReportsAccountsGo() {
     MTP.Column = 1; MTP.Title = 'Type'; MF_QueueAddTitle(MTP);
     MTP.Column = 2; MTP.Title = 'Updated';MF_QueueAddTitle(MTP);
     MTP.Column = 3; MTP.Title = 'Beginning Balance'; MTP.isSortable = 2; MTP.Format = 1;MF_QueueAddTitle(MTP);
-    if(MTFlex.Button2 > 1) { MTP.isHidden = true; }
+    if(MTFlex.Button2 > 2) { MTP.isHidden = true; }
     MTP.Column = 4; MTP.Title = 'Income'; MF_QueueAddTitle(MTP);
     MTP.Column = 5; MTP.Title = 'Expenses'; MF_QueueAddTitle(MTP);
     MTP.Column = 6; MTP.Title = 'Transfers'; MF_QueueAddTitle(MTP);
@@ -692,13 +692,13 @@ async function MenuReportsAccountsGo() {
     let pastBalance = 0;
     let useAmount = 0;
     let skipTxs = getCookie('MT_AccountsBalance',true);
-    if(MTFlex.Button2 > 1) {skipTxs = 0;} else {snapshotData2 = await GetTransactions(formatQueryDate(useDate),formatQueryDate(useDate2),0);}
+    if(MTFlex.Button2 > 2) {skipTxs = 0;} else {snapshotData2 = await GetTransactions(formatQueryDate(useDate),formatQueryDate(useDate2),0);}
 
     snapshotData = await getAccountsData();
     snapshotData3 = await getDisplayBalanceAtDateData(formatQueryDate(useDate));
 
     for (let i = 0; i < snapshotData.accounts.length; i += 1) {
-        if(snapshotData.accounts[i].isHidden == false ) {
+
             MTP = [];
             MTP.isHeader = false;
             MTP.UID = snapshotData.accounts[i].id;
@@ -736,7 +736,7 @@ async function MenuReportsAccountsGo() {
                 MTFlexRow[MTFlexCR][MTFields+7] = useBalance;
                 MTFlexRow[MTFlexCR][MTFields+8] = 0;
                 if(snapshotData.accounts[i].hideTransactionsFromReports == false) {
-                    if(MTFlex.Button2 < 2) {
+                    if(MTFlex.Button2 < 3) {
                         for (let j = 0; j < snapshotData2.allTransactions.results.length; j += 1) {
                             if(snapshotData2.allTransactions.results[j].hideFromReports == false && snapshotData2.allTransactions.results[j].pending == false) {
                                 if(snapshotData2.allTransactions.results[j].account.id == snapshotData.accounts[i].id) {
@@ -781,7 +781,7 @@ async function MenuReportsAccountsGo() {
                     cards+=1;
                 }
             }
-        }
+
     }
     MT_GridRollup(1,2,1,'Assets');
     MT_GridRollup(3,4,2,'Liabilities');
@@ -2032,6 +2032,7 @@ function getDates(InValue,InDate) {
     if(InValue == 'n_CurMonth') {return(month);}
     if(InValue == 'n_CurDay') {return(day);}
     if(InValue == 'd_CurDate') {return d;}
+    if(InValue == 'd_Yesterday') {d.setDate(d.getDate() - 1);return d;}
     if(InValue == 'd_MinusWeek') {d.setDate(d.getDate() - 7);return d;}
     if(InValue == 's_FullDate') {return(getMonthName(month,true) + ' ' + day + ', ' + year );}
     if(InValue == 's_ShortDate') {return(getMonthName(month,true) + ' ' + day);}
