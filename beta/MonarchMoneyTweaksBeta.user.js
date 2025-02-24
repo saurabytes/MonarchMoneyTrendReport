9908f0f96fffc13c4e68e2600b107196616c2c10
Robert P <163090599+RobertParesi@users.noreply.github.com>
2025-02-24 08:50:11 -0800

    Update MonarchMoneyTweaksBeta.user.js
    Signed-off-by: Robert P <163090599+RobertParesi@users.noreply.github.com>

--- beta/MonarchMoneyTweaksBeta.user.js
+++ beta/MonarchMoneyTweaksBeta.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         Monarch Money Tweaks
 // @namespace    http://tampermonkey.net/
-// @version      2.32.02
+// @version      2.32
 // @description  Monarch Tweaks
 // @author       Robert P
 // @match        https://app.monarchmoney.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=monarchmoney.com
 // ==/UserScript==
 
-const version = '2.32.02';
+const version = '2.32';
 const css_currency = 'USD';
 const css_green = 'color: #2a7e3b;',css_red = 'color: #d13415;';
 const graphql = 'https://api.monarchmoney.com/graphql';
