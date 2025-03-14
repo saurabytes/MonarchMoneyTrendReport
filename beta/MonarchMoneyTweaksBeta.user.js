56eca491801e2b4e6555cf7c6230940d157e638e
Robert P <163090599+RobertParesi@users.noreply.github.com>
2025-03-13 18:48:38 -0700

    Update MonarchMoneyTweaksBeta.user.js
    Signed-off-by: Robert P <163090599+RobertParesi@users.noreply.github.com>

--- beta/MonarchMoneyTweaksBeta.user.js
+++ beta/MonarchMoneyTweaksBeta.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         Monarch Money Tweaks
 // @namespace    http://tampermonkey.net/
-// @version      3.00.05
+// @version      3.00
 // @description  Monarch Tweaks
 // @author       Robert P
 // @match        https://app.monarchmoney.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=monarchmoney.com
 // ==/UserScript==
 
-const version = '3.00.05';
+const version = '3.00';
 const css_currency = 'USD';
 const css_green = 'color: #2a7e3b;',css_red = 'color: #d13415;';
 const graphql = 'https://api.monarchmoney.com/graphql';
@@ -769,7 +769,6 @@ async function MenuReportsAccountsGoExt(){
     MTP.isHidden = false;
     MTP.Column = 15; MTP.Title = 'Current';MF_QueueAddTitle(MTP);
     MTP.Column = 16; MTP.Title = 'Average';MF_QueueAddTitle(MTP);
-hhh-0;
     snapshotData = await getAccountsData();
     if(debug == 1) console.log('MenuReportsAccountsGoExt',snapshotData,MTFlex.Subtotals);
     for (let i = 0; i < snapshotData.accounts.length; i += 1) {
